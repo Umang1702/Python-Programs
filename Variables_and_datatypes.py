5cf15bc6f8a2d62456c63692fcfb342fe8a18232
Umang Sharma <usharma1702@gmail.com>
2025-07-17 15:22:36 +0530

    Initial Commit

--- Variables_and_datatypes.py
+++ Variables_and_datatypes.py
@@ -1,6 +1,41 @@
-# Question 1: Write a python program to add two numbers and take both number as a input from the users.
-a=int(input("Enter your first number:"))
-b=int(input("Enter your second number:"))
-sum=a+b
-print("The Addition of both numbers are: ",sum)
+# # Question 1: Write a python program to add two numbers and take both number as a input from the users.
+# a=int(input("Enter your first number:"))
+# b=int(input("Enter your second number:"))
+# sum=a+b
+# print("The Addition of both numbers are: ",sum)
 
+
+
+# # Question 2:Write a python program to find the remainder when a number divided by z.
+# z=10
+# print("Remainder after divided by z is :",78/z)
+
+
+
+# # Check the type of variable assigned using input() function.
+# a=input()
+# print(type(a))
+
+
+
+
+# # Use comparision operator to find out whether 'a' given variable is a greater then 'b' or not.Take a=34 and b=80.
+# a=34 
+# b=80
+# print("a is greater than b:",a>b)
+
+
+
+
+# # Write a python program to find an average of two numbers entered by the user.
+# a=int(input("Enter a first number :"))
+# b=int(input("Enter a second number :"))
+# print("The Average of two numbers is :",a+b/2)
+
+
+
+
+
+# # Write a python program to calculate the square of a number entered by the user.
+# a=int(input("Enter your number:"))
+# print("The square of a given number is :",a**2)
