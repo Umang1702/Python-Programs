52f7fbac257024163d7233f17ac1ea3cd4e26000
Umang Sharma <usharma1702@gmail.com>
2025-07-17 14:56:55 +0530

    Initial Commit

---
+++ Variables_and_datatypes.py
@@ -0,0 +1,6 @@
+# Question 1: Write a python program to add two numbers and take both number as a input from the users.
+a=int(input("Enter your first number:"))
+b=int(input("Enter your second number:"))
+sum=a+b
+print("The Addition of both numbers are: ",sum)
+
